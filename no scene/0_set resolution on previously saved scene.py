d573c73a3602935fcff4170fe321d1f20b4546e7
Zeyl <jeffzeyl@sun.ac.za>
2020-09-30 19:33:25 +0200

    sept 30

---
+++ no scene/0_set resolution on previously saved scene.py
@@ -0,0 +1,48 @@
+
+#set up resolution
+masterVolumeNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
+import itertools
+imagespacing = [spacing]*3
+masterVolumeNode.SetSpacing(imagespacing)#assign resolution to the volume
+volumeScalarRange = masterVolumeNode.GetImageData().GetScalarRange()
+
+#set up segmentatin node
+segmentationNode = slicer.util.getNode('Segmentation')
+
+segmentationDisplayNode=segmentationNode.GetDisplayNode()
+#if previously saved
+segmentEditorNode = slicer.util.getNode('SegmentEditor')
+
+# Create segment editor to get access to effects
+segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+segmentEditorWidget.setMRMLScene(slicer.mrmlScene)#connect widget to scene
+segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)#connect segment editor to editor widget
+segmentEditorWidget.setSegmentationNode(segmentationNode)#connect segmentation node
+segmentEditorWidget.setMasterVolumeNode(masterVolumeNode)#connect master node
+
+# Compute bone threshold value automatically
+import vtkITK
+ME_thresholdCalculator = vtkITK.vtkITKImageThresholdCalculator()
+ME_thresholdCalculator.SetInputData(masterVolumeNode.GetImageData())
+ME_thresholdCalculator.SetMethodToMaximumEntropy()
+ME_thresholdCalculator.Update()
+Maxentval = ME_thresholdCalculator.GetThreshold()
+
+ISO_thresholdCalculator = vtkITK.vtkITKImageThresholdCalculator()
+ISO_thresholdCalculator.SetInputData(masterVolumeNode.GetImageData())
+ISO_thresholdCalculator.SetMethodToIsoData()
+ISO_thresholdCalculator.Update()
+ISOval = ISO_thresholdCalculator.GetThreshold()
+
+FIDNode1 = getNode(ID+" TM")
+FIDNode2 = getNode(ID+" RW")
+FIDNode3 = getNode(ID+" CA")
+FIDNode4 = getNode(ID+" EC")
+
+FIDNode5 = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsFiducialNode")
+FIDNode5.SetName(ID+"ECandTMmrk_outline")#creates a new segmentation
+
+FIDNode5 = getNode(ID+"ECandTMmrk_outline")
+
+
+
